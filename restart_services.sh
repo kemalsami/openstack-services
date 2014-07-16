0464b8368bf4561bb6d7d8447c4f664a901bdd17
kemal.sami <kemalsami.karaca@tubitak.gov.tr>
2014-07-16 17:00:28 +0300

    -1-

--- restart_services.sh
+++ restart_services.sh
@@ -1,14 +1,59 @@
+#!/bin/bash
+
+INIT_DIRECTORY=`pwd`
+SERVICE="ALL"
+OPERATION="status"
+
+###########################################
+###########################################
+##
+## --1-- 
+##
+###########################################
+###########################################
+printusage()
+{
+        echo "This script interferes OpenStack services which are nova, neutron, glance and so on"
+	echo "-s parameter for services name"
+	echo "-o name of operation{restart|start|stop|status}"
+	exit ${EXITPROB}
+}
+
+#parse the arguments
+while getopts ":hs:s:o:" opt
+do
+        case $opt in
+                h ) printusage;;
+                s ) SERVICE=${OPTARG};;
+		o ) OPERATION=${OPTARG};;
+                ? ) printusage;;	
+        esac
+done
+
+###########################################
+###########################################
+##
+## --2-- 
+##
+###########################################
+###########################################
 cd /etc/init.d/ ;
 
-#service libvirt-bin restart;
 
 for i in $(ls neutron-*);
-do service $i restart
+do service $i $OPERATION
 done
 
 for i in $(ls nova-*);
-do service $i restart
+do service $i $OPERATION
 done
 
-cd /
+###########################################
+###########################################
+##
+## --3-- return user directory 
+##
+###########################################
+###########################################
+cd $INIT_DIRECTORY
 
